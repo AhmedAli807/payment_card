75ef1a63b865a23276795ebbb9f962bc566eeb88
Ahmed Ali <ahmed@domar.cloud>
2024-08-29 00:45:31 +0300

    Update screen_size.dart

--- lib/config/screen_size.dart
+++ lib/config/screen_size.dart
@@ -3,4 +3,5 @@ import 'package:flutter/cupertino.dart';
 abstract class ScreenSize{
   static double height(BuildContext context)=>MediaQuery.of(context).size.height;
   static double width(BuildContext context)=>MediaQuery.of(context).size.height;
+  static double widtho(BuildContext context)=>MediaQuery.of(context).size.height;
 }
